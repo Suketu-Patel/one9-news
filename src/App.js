9b1471b9e1b46ffe8baeaf4224ef7ec26f096a7d
Smit Patel <smit.patel@savvas.com>
2020-10-03 22:58:35 +0530

    name change

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import api from './api'
 import './App.css';
 
 const App = () => {
-  const [result, setResult] = useState({});
+  const [newsData, setNewsData] = useState({});
   useEffect(() => {
     fetchResult();
   }, [])
 
   const fetchResult = async () => {
-    const result = await api.get("q=ipl")
-    setResult(result.articles)
+    const newsData = await api.get("q=ipl")
+    setNewsData(newsData.articles)
   }
 
   return (
     <div className="App">
       {
-        result && <pre><code>{JSON.stringify(result, null, 4)}</code></pre>
+        newsData && <pre><code>{JSON.stringify(newsData, null, 4)}</code></pre>
       }
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card(props) {
+  return (
+    <div>
+      card workihng.. 
+    </div>
+  )
+}
+
+export default Card;
