e57e261e1ccc8904c66b95618d71f393bc55f50d
Suketu.P <suketu.p@simformsolutions.com>
2020-10-03 22:16:36 +0530

    add api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_OMDB_API_KEY}`
+const App = () => {
+  const [result, setResult] = useState({});
+  useEffect(() => {
+    fetchResult();
+  }, [])
+
+  const fetchResult = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    setResult(data)
+  }
+
   return (
     <div className="App">
-     starting .... .
+      {
+        result && <pre><code>{JSON.stringify(result, null, 4)}</code></pre>
+      }
+      {console.log(result)}
     </div>
   );
 }
