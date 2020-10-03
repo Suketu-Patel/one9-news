115901207fcae089fa56249e2320b7224c68cfad
Smit Patel <smit.patel@savvas.com>
2020-10-04 03:52:09 +0530

    add comments and code formatting

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { Card } from 'antd';
-import { Link } from "@reach/router"
-import './styles.css';
+import React from "react";
+import { Card } from "antd";
+import { Link } from "@reach/router";
+import "./styles.css";
 
 const { Meta } = Card;
 
 function CardComponent(props) {
-  const { id, source, author, title, description, urlToImage, url, content } = props.news;
+  const {
+    id,
+    source,
+    author,
+    title,
+    description,
+    urlToImage,
+    url,
+    content,
+  } = props.news; // here we have used javascript destructuring concept.....
+
   return (
     <Link to={`details/${id}`} state={{ data: props.news }}>
       <Card
@@ -18,7 +28,7 @@ function CardComponent(props) {
         <Meta title={title} description={description} />
       </Card>
     </Link>
-  )
+  );
 }
 
 export default CardComponent;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,19 @@
-import React from "react"
-import Card from "./Card/Card"
-import { Col, Row } from "antd"
+import React from "react";
+import Card from "./Card/Card";
+import { Col, Row } from "antd";
 const Cards = ({ newsData = [] }) => {
-    return (
-        <div>
-            <Row gutter={8}>
-                {
-                    newsData.map(singleNews => {
-                        return <Col xs={16} xl={10} xxl={6}><Card news={singleNews} /></Col>
-                    })
-                }
-            </Row>
-        </div>
-    )
-}
-export default Cards
+  return (
+    <div>
+      <Row gutter={8}>
+        {newsData.map((singleNews) => {
+          return (
+            <Col xs={16} xl={10} xxl={6}>
+              <Card news={singleNews} />
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
+};
+export default Cards;
